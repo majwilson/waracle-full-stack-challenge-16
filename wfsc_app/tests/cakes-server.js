75b7ea97e67dfd82cf2a7f941285467ed7d9f4a7
Mark Wilson <majwilson@gmail.com>
2021-06-29 12:53:27 +0100

    start get cake by name tests

--- wfsc_app/tests/cakes-server.js
+++ wfsc_app/tests/cakes-server.js
@@ -1,9 +1,14 @@
-import { Dummy } from '../imports/api/CakesPublication';
+import '../imports/api/CakesPublication';
 import { CakesCollection } from '../imports/db/CakesCollection';
 
 import { expect } from 'chai';
 import fetch from "node-fetch";
 
+/* global Meteor */
+
+const jsonReq = async ( url ) => {
+  return await fetch( url, { compress: false, headers: { Accept: 'application/json' } } );
+};
 
 describe( "cakes are served", () => {
   beforeEach( () => {
@@ -16,7 +21,7 @@ describe( "cakes are served", () => {
     } );
   } );
 
-  it( "returns a list of cakes", async () => {
+  it( "returns-a-list-of-cakes", async () => {
     let url = Meteor.absoluteUrl( '/cakes' );
     // console.log( "CAKES ARE SERVED", url );
     let rslt = await fetch( url, { compress: false, headers: { Accept: 'application/json' } } );
@@ -26,6 +31,18 @@ describe( "cakes are served", () => {
     expect( cakes_list.length ).to.eql( 2 );
     expect( cakes_list[ 0 ].name ).to.eql( "Blue Cake" );
     expect( cakes_list[ 1 ].yumFactor ).to.eql( 3 );
+  });
+
+  it.only( "gets-a-cake-by-name", async () => {
+    let cake_name = "Blue Cake";
+    let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
+    // console.log( "A CAKE IS FOUND", url );
+    let rslt = await jsonReq( url );
+    expect( rslt.status ).to.eql( 200 );
+    let cakes_list = ( await rslt.json() ).cakes;
+    // console.log( "cakes_list", cakes_list );
+    expect( cakes_list.length ).to.eql( 1 );
+    expect( cakes_list[ 0 ].name ).to.eql( "Blue Cake" );
   });
 
 });
