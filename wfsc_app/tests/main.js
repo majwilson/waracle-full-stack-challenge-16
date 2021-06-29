b241194d5e9abb65d05397b279a5969e6ed6c673
Mark Wilson <majwilson@gmail.com>
2021-06-29 12:23:11 +0100

    test for /cakes ok

--- wfsc_app/tests/main.js
+++ wfsc_app/tests/main.js
@@ -21,18 +21,32 @@ describe("wfsc_app", function () {
   }
 });
 
+
 import { Dummy } from '../imports/api/CakesPublication';
+import { CakesCollection } from '../imports/db/CakesCollection';
 
 describe( "cakes are served", () => {
+  beforeEach( () => {
+    CakesCollection.insert( {
+      name: "Blue Cake"
+    } );
+
+    CakesCollection.insert( {
+      name: "Yellow Cake"
+    } );
+  } );
+
   it( "returns a list of cakes", async () => {
 
     let url = Meteor.absoluteUrl( '/cakes' );
-    console.log( "CAKES ARE SERVED", url );
+    // console.log( "CAKES ARE SERVED", url );
     let rslt = await fetch( url, { compress: false, headers: { Accept: 'application/json' } } );
     expect( rslt.status ).to.eql( 200 );
-    let cakes_list = await rslt.json();
-    console.log( "cakes_list", cakes_list );
-    expect( cakes_list ).to.eql( {} );
+    let cakes_list = ( await rslt.json() ).cakes;
+    // console.log( "cakes_list", cakes_list );
+    expect( cakes_list.length ).to.eql( 2 );
+    expect( cakes_list[ 0 ].name ).to.eql( "Blue Cake" );
+    expect( cakes_list[ 1 ].yumFactor ).to.eql( 3 );
   });
 
 });
