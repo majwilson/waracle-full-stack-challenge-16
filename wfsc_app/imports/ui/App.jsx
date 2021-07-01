25bd6711ab10a4af8eac22d09916bfd75982d686
Mark Wilson <majwilson@gmail.com>
2021-07-01 16:29:24 +0100

    App component gets cakes from server

--- wfsc_app/imports/ui/App.jsx
+++ wfsc_app/imports/ui/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // import { YumIndicator } from './YumIndicator.jsx';
 // import { CakesListItem } from './CakesListItem.jsx';
@@ -6,21 +6,38 @@ import { CakesList } from './CakesList.jsx';
 import { CakeForm } from './CakeForm.jsx';
 import { PopUp } from './PopUp.jsx';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Welcome to Cakes!</h1>
-        <CakesList
-          cakes={ [] }
+
+export const App = ( props ) => {
+  let [ all_cakes, set_all_cakes ] = useState( [] );
+  all_cakes = all_cakes || [];
+  let [ selected_cake, set_selected_cake ] = useState( null );
+  let [ popup_visible, set_popup_visible ] = useState( false );
+  let [ cake_editable, set_cake_editable ] = useState( false );
+
+  useEffect( () => {
+    fetchAllCakes = async () => {
+      console.log( "Fetching cakes" );
+      const result = await fetch( '/cakes', { headers: { Accept: 'application/json', } } );
+      let _all_cakes = ( await result.json() ).cakes;
+      console.log( "GOT cakes", _all_cakes );
+      set_all_cakes( _all_cakes );
+    }
+    fetchAllCakes();
+  }, [] );
+
+  return (
+    <div>
+      <h1>Welcome to Cakes!</h1>
+      <CakesList
+        className="CakesList"
+        cakes={ all_cakes }
+      />
+      <PopUp
+        visible={ false } >
+        <CakeForm
+          editable={ false }
         />
-        <PopUp
-          visible={ false } >
-          <CakeForm
-            editable={ false }
-          />
-        </PopUp>
-      </div>
-    )
-  }
+      </PopUp>
+    </div>
+  )
 }
