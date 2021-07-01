8ec9c4e98591fca15828f65879a8287c3c82d12c
Mark Wilson <majwilson@gmail.com>
2021-07-01 16:28:17 +0100

    move test cakes_list into fixtures.js

--- wfsc_app/tests/test-client-app.js
+++ wfsc_app/tests/test-client-app.js
@@ -1,17 +1,34 @@
 import 'jsdom-global/register';
 import React from 'react';
 import { expect } from 'chai';
+import fetchMock from 'fetch-mock';
 import { configure, shallow, mount } from 'enzyme';
+import waitUntil from 'async-wait-until';
 
 import Adapter from 'enzyme-adapter-react-16';
 configure( { adapter: new Adapter() } );
 
 import { App } from '../imports/ui/App.jsx';
 
+import { cakes_list } from './fixtures';
+
 
 describe( 'App', () => {
-  it.only('should-render', () => {
-    const wrap = mount(<App />);
-    console.log( wrap.debug() );
+  before( () => {
+    fetchMock.get( '/cakes', { cakes: cakes_list } );
+  } );
+
+  it.only('should-render', async () => {
+    const wrap = await mount(<App />);
+    wrap.update();
+    let count = 20;
+    const interval = setInterval( () => {
+      console.log( "________" );
+      if( count-- < 0 ) {
+        clearInterval( interval );
+      }
+      console.log( wrap.debug() );
+    }, 100 );
+    await waitUntil( () => count < 1 );
   });
 });
--- wfsc_app/tests/test-client-components.js
+++ wfsc_app/tests/test-client-components.js
@@ -12,29 +12,6 @@ import { CakeForm } from '../imports/ui/CakeForm.jsx';
 import { PopUp } from '../imports/ui/PopUp.jsx';
 
 
-let cakes_list = [];
-cakes_list.push( {
-  _id: 111,
-  name: 'Blue Cake',
-  comment: 'this cake is blue',
-  imageUrl: '/blue-cake.jpg',
-  yumFactor: 2,
-} );
-cakes_list.push( {
-  _id: 222,
-  name: 'Red Cake',
-  comment: 'this cake is red',
-  imageUrl: '/red-cake.jpg',
-  yumFactor: 0,
-} );
-cakes_list.push( {
-  _id: 333,
-  name: 'Yellow Cake',
-  comment: 'this cake is yellow',
-  imageUrl: '/yellow-cake.jpg',
-  yumFactor: 0,
-} );
-
 
 describe( 'YumIndicator', () => {
   it('should-render-2-as-yum-yum', () => {
