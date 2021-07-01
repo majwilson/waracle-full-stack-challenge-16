e6e1bbcc7fb9df6f2926890c3eab5a0227bcb8fc
Mark Wilson <majwilson@gmail.com>
2021-07-01 16:28:57 +0100

    add some default cakes at startup if db empty

--- wfsc_app/server/main.js
+++ wfsc_app/server/main.js
@@ -1,31 +1,25 @@
 import { Meteor } from 'meteor/meteor';
-import { LinksCollection } from '/imports/api/links';
+import '/imports/api/CakesPublication';
+import { CakesCollection } from '/imports/db/CakesCollection';
 
-function insertLink({ title, url }) {
-  LinksCollection.insert({title, url, createdAt: new Date()});
+function addCake( name, imageUrl, yumFactor=0, comment='' ) {
+  CakesCollection.insert( { name, comment, yumFactor, imageUrl, } );
 }
 
 Meteor.startup(() => {
-  // If the Links collection is empty, add some data.
-  if (LinksCollection.find().count() === 0) {
-    insertLink({
-      title: 'Do the Tutorial',
-      url: 'https://www.meteor.com/tutorials/react/creating-an-app'
-    });
+  if( CakesCollection.find().count() === 0) {
+    addCake( 'Dundee Cake', '/Dundee-Cake.jpeg', 2, '' );
+    addCake( 'Easter Chocolate Cake', '/Easter-Chocolate-Cake.jpg', 1, '' );
+    addCake( 'Fish Cake', '/Fish-Cake.png', -1, 'Hmmmmmmmm' );
+    addCake( 'Hamburger Cake', '/Hamburger-Cake.jpg', 0, 'looks savoury' );
+    addCake( 'Lemon Layer Cake', '/Lemon-Layer-Cake.jpg', 1, '' );
+    addCake( 'Maltesers Cake', '/Maltesers-Cake.jpeg', 1, '' );
+    addCake( 'Number Five Cake', '/Number-Five-Cake.jpeg', 1, 'numeric' );
+    addCake( 'Peppa Pig Birthday Cake', '/Peppa-Pig-Birthday-Cake.jpeg', 1, '' );
+    addCake( 'Purple Yam Cake', '/Purple-Yam-Cake.jpg', 1, '' );
+    addCake( 'Rainbow Cake', '/Rainbow-Cake.jpg', 1, '' );
+    addCake( 'Swiss Roll', '/Swiss-Roll.png', 1, 'is it a cake?' );
+    addCake( 'Yellow Cake', '/Yellow-Cake.png', -2, '' );
+    }
+} );
 
-    insertLink({
-      title: 'Follow the Guide',
-      url: 'http://guide.meteor.com'
-    });
-
-    insertLink({
-      title: 'Read the Docs',
-      url: 'https://docs.meteor.com'
-    });
-
-    insertLink({
-      title: 'Discussions',
-      url: 'https://forums.meteor.com'
-    });
-  }
-});
