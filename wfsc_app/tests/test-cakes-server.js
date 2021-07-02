f8cbba8766c48815ec1ad0060b5208d64c213498
Mark Wilson <majwilson@gmail.com>
2021-07-02 08:44:00 +0100

    tidy up test-cakes-server.js

--- wfsc_app/tests/test-cakes-server.js
+++ wfsc_app/tests/test-cakes-server.js
@@ -6,10 +6,16 @@ import fetch from "node-fetch";
 
 /* global Meteor */
 
-const jsonReq = async ( url ) => {
-  return await fetch( url, { compress: false, headers: { Accept: 'application/json' } } );
+const jsonReq = async ( url, method, body ) => {
+  return await fetch( url, {
+      method,
+      body,
+      compress: false,
+      headers: { Accept: 'application/json' }
+    } );
 };
 
+// -------------------------------------------------------------------------------------------------
 describe( "cakes are served", () => {
   beforeEach( () => {
     CakesCollection.remove( {} );
@@ -21,71 +27,70 @@ describe( "cakes are served", () => {
     } );
   } );
 
-  it( "returns-a-list-of-cakes", async () => {
-    let url = Meteor.absoluteUrl( '/cakes' );
-    let rslt = await fetch( url, { compress: false, headers: { Accept: 'application/json' } } );
-    expect( rslt.status ).to.eql( 200 );
-    let cakes_list = ( await rslt.json() ).cakes;
-    expect( cakes_list.length ).to.eql( 2 );
-    expect( cakes_list[ 0 ].name ).to.eql( "Blue Cake" );
-    expect( cakes_list[ 1 ].yumFactor ).to.eql( 0 );
-  } );
-
-  it( "gets-a-cake-by-name", async () => {
-    let cake_name = "Blue Cake";
-    let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
-    let rslt = await jsonReq( url );
-    expect( rslt.status ).to.eql( 200 );
-    let cakes_list = ( await rslt.json() ).cakes;
-    expect( cakes_list.length ).to.eql( 1 );
-    expect( cakes_list[ 0 ].name ).to.eql( "Blue Cake" );
-  } );
-
-  it( "raises-404-if-it-fails-to-get-a-cake-by-name", async () => {
-    let cake_name = "ORANGE Cake";
-    let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
-    let rslt = await jsonReq( url );
-    expect( rslt.status ).to.eql( 404 );
-  } );
-
-  it( "adds-a-new-cake", async () => {
-    let cake_info = {
-      name: 'New Cake',
-      comment: 'this is a comment',
-      imageUrl: '/url/to/cake.jpg',
-      yumFactor: 5,
-    };
-    let url = Meteor.absoluteUrl( `/cakes` );
-    let rslt = await fetch( url, {
-      method: 'post',
-      body: new URLSearchParams( cake_info ),
-      compress: false,
-      headers: { Accept: 'application/json', contentType: 'application/x-www-form-urlencoded' }
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  describe( "get-a-list", () => {
+    it( "returns-a-list-of-cakes", async () => {
+      let url = Meteor.absoluteUrl( '/cakes' );
+      let rslt = await jsonReq( url );
+      expect( rslt.status ).to.eql( 200 );
+      let cakes_list = ( await rslt.json() ).cakes;
+      expect( cakes_list.length ).to.eql( 2 );
+      expect( cakes_list[ 0 ].name ).to.eql( "Blue Cake" );
+      expect( cakes_list[ 1 ].yumFactor ).to.eql( 0 );
     } );
-    expect( rslt.status ).to.eql( 200 );
-    expect( CakesCollection.find( {} ).count() ).to.eql( 3 );
-    expect( CakesCollection.findOne( { name: cake_info.name } ).name ).to.eql( cake_info.name );
   } );
 
-  it( "raises-422-if-adding-a-cake-with-duplicate-name", async () => {
-    let cake_info = {
-      name: "Blue Cake",
-      comment: 'this is a comment',
-      imageUrl: '/url/to/cake.jpg',
-      yumFactor: 5,
-    };
-    let url = Meteor.absoluteUrl( `/cakes` );
-    let rslt = await fetch( url, {
-        method: 'post',
-        body: new URLSearchParams( cake_info ),
-        compress: false,
-        headers: { Accept: 'application/json', contentType: 'application/x-www-form-urlencoded' }
-      }
-    );
-    expect( rslt.status ).to.eql( 422 );
-    expect( CakesCollection.find( {} ).count() ).to.eql( 2 );
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  describe( "get-a-cake", () => {
+    it( "gets-a-cake-by-name", async () => {
+      let cake_name = "Blue Cake";
+      let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
+      let rslt = await jsonReq( url );
+      expect( rslt.status ).to.eql( 200 );
+      let cakes_list = ( await rslt.json() ).cakes;
+      expect( cakes_list.length ).to.eql( 1 );
+      expect( cakes_list[ 0 ].name ).to.eql( "Blue Cake" );
+    } );
+
+    it( "raises-404-if-it-fails-to-get-a-cake-by-name", async () => {
+      let cake_name = "ORANGE Cake";
+      let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
+      let rslt = await jsonReq( url );
+      expect( rslt.status ).to.eql( 404 );
+    } );
   } );
 
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  describe( "post (add)", () => {
+    it( "adds-a-new-cake", async () => {
+      let cake_info = {
+        name: 'New Cake',
+        comment: 'this is a comment',
+        imageUrl: '/url/to/cake.jpg',
+        yumFactor: 5,
+      };
+      let url = Meteor.absoluteUrl( `/cakes` );
+      let rslt = await jsonReq( url, 'post', new URLSearchParams( cake_info ) );
+      expect( rslt.status ).to.eql( 200 );
+      expect( CakesCollection.find( {} ).count() ).to.eql( 3 );
+      expect( CakesCollection.findOne( { name: cake_info.name } ).name ).to.eql( cake_info.name );
+    } );
+
+    it( "raises-422-if-adding-a-cake-with-duplicate-name", async () => {
+      let cake_info = {
+        name: "Blue Cake",
+        comment: 'this is a comment',
+        imageUrl: '/url/to/cake.jpg',
+        yumFactor: 5,
+      };
+      let url = Meteor.absoluteUrl( `/cakes` );
+      let rslt = await jsonReq( url, 'post', new URLSearchParams( cake_info ) );
+      expect( rslt.status ).to.eql( 422 );
+      expect( CakesCollection.find( {} ).count() ).to.eql( 2 );
+    } );
+  } );
+
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
   describe( "put (update)", () => {
     it( "updates-a-new-cake", async () => {
       let cake_info = {
@@ -95,12 +100,7 @@ describe( "cakes are served", () => {
         yumFactor: 5,
       };
       let url = Meteor.absoluteUrl( `/cakes` );
-      let rslt = await fetch( url, {
-        method: 'put',
-        body: new URLSearchParams( cake_info ),
-        compress: false,
-        headers: { Accept: 'application/json', contentType: 'application/x-www-form-urlencoded' }
-      } );
+      let rslt = await jsonReq( url, 'put', new URLSearchParams( cake_info ) );
       expect( rslt.status ).to.eql( 200 );
       expect( CakesCollection.find( {} ).count() ).to.eql( 2 );
       expect( CakesCollection.findOne( { name: cake_info.name } ).comment ).to.eql( cake_info.comment );
@@ -113,37 +113,29 @@ describe( "cakes are served", () => {
         yumFactor: 5,
       };
       let url = Meteor.absoluteUrl( `/cakes` );
-      let rslt = await fetch( url, {
-          method: 'put',
-          body: new URLSearchParams( cake_info ),
-          compress: false,
-          headers: { Accept: 'application/json', contentType: 'application/x-www-form-urlencoded' }
-        }
-      );
+      let rslt = await jsonReq( url, 'put', new URLSearchParams( cake_info ) );
       expect( rslt.status ).to.eql( 404 );
       expect( CakesCollection.find( {} ).count() ).to.eql( 2 );
     } );
   } );
 
-
-  it( "deletes-a-cake", async () => {
-    let cake_name = "Blue Cake";
-    let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
-    let rslt = await fetch( url, {
-      method: 'delete',
+  // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+  describe( "delete", () => {
+    it( "deletes-a-cake", async () => {
+      let cake_name = "Blue Cake";
+      let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
+      let rslt = await jsonReq( url, 'delete' );
+      expect( rslt.status ).to.eql( 200 );
+      expect( CakesCollection.find( {} ).count() ).to.eql( 1 );
     } );
-    expect( rslt.status ).to.eql( 200 );
-    expect( CakesCollection.find( {} ).count() ).to.eql( 1 );
-  } );
 
-  it( "does-nothing-if-deleting-a-cake-that-is-not-there", async () => {
-    let cake_name = "ORANGE Cake";
-    let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
-    let rslt = await fetch( url, {
-      method: 'delete',
+    it( "does-nothing-if-deleting-a-cake-that-is-not-there", async () => {
+      let cake_name = "ORANGE Cake";
+      let url = Meteor.absoluteUrl( `/cakes/${ cake_name }` );
+      let rslt = await jsonReq( url, 'delete' );
+      expect( rslt.status ).to.eql( 200 );
+      expect( CakesCollection.find( {} ).count() ).to.eql( 2 );
     } );
-    expect( rslt.status ).to.eql( 200 );
-    expect( CakesCollection.find( {} ).count() ).to.eql( 2 );
   } );
 });
 
